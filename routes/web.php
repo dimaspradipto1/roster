6f16d627b4c6b890e3dbf59185d1d3eccf05fdf5
dimas pradipto <dpradipto8@gmail.com>
2026-06-16 12:48:06 +0700

    feat: implement authentication controllers, routes, and login view layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/', 'login')->name('login');
+    Route::post('loginproses', 'loginproses')->name('loginproses');
+    Route::get('logout', 'logout')->name('logout');
 });
