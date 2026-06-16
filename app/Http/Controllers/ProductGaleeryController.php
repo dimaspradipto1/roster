9f56c1c3fa3b0ca89117c86174e6340493c25161
dimas pradipto <dpradipto8@gmail.com>
2026-06-16 12:18:09 +0700

    feat: initialize core models, controllers, and database migrations for application schema

---
+++ app/Http/Controllers/ProductGaleeryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductGaleery;
+use Illuminate\Http\Request;
+
+class ProductGaleeryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductGaleery $productGaleery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductGaleery $productGaleery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductGaleery $productGaleery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductGaleery $productGaleery)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_06_16_051136_create_profils_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profils', function (Blueprint $table) {
+            $table->id();
+            $table->string('logo')->nullable();
+            $table->string('nama_perusahaan')->nullable();
+            $table->string('nama_pemilik')->nullable();
+            $table->string('no_telp')->nullable();
+            $table->text('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profils');
+    }
+};
